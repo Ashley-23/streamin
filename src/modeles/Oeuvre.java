a1ccbc4fbd24ccde695ec49e81995d83b739fe2d
Ashley-23 <ashleysatchivi92@gmail.com>
2024-05-13 16:30:19 +0200

    ajout de la table oeuvre

--- src/modeles/Oeuvre.java
+++ src/modeles/Oeuvre.java
@@ -1,4 +1,68 @@
 package modeles;
 
 public class Oeuvre {
+
+    public int id;
+    public String nom;
+    public String description;
+    public String acteurs[];
+    public int age;
+    public String bande_d_annonce;
+    public int duree_download;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String[] getActeurs() {
+        return acteurs;
+    }
+
+    public void setActeurs(String[] acteurs) {
+        this.acteurs = acteurs;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBande_d_annonce() {
+        return bande_d_annonce;
+    }
+
+    public void setBande_d_annonce(String bande_d_annonce) {
+        this.bande_d_annonce = bande_d_annonce;
+    }
+
+    public int getDuree_download() {
+        return duree_download;
+    }
+
+    public void setDuree_download(int duree_download) {
+        this.duree_download = duree_download;
+    }
 }
